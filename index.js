949de5ad8bbc6cd4ae895369a8d83467f820dff3
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-12-09 17:41:14 +0000

    RawProvider (sort of) working

--- index.js
+++ index.js
@@ -1,38 +1,85 @@
 var Tx = require('ethereumjs-tx'),
     BN = require('bn.js'),
+    _ = require("lodash"),
     ethUtils = require("ethereumjs-util"),
     Web3 = require("web3");
 
 
+var HttpProvider = Web3.providers.HttpProvider;
 
-var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://46.101.74.242:8545'));
+var RawProvider = function(host, pk){
+  if(!pk) throw new Error("Private key must be provided");
+  
+  HttpProvider.call(this, host);
 
-console.log("block:", web3.eth.getBlock("latest"));
+  this._address = ethUtils.privateToAddress(pk).toString("hex");
 
-var privateKey = new Buffer('e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109',
-                            'hex');
-var address = ethUtils.privateToAddress(privateKey).toString("hex");
+  this.setPrivateKey(pk);
+};
 
-var sendAmount  = 100000,
-    hexAmt = '0' + sendAmount.toString(16);
+RawProvider.prototype = _.extend({}, HttpProvider.prototype, {
+  setPrivateKey: function(pk){
+    this._pk = pk;
+    this._updateNonce();
+  },
+  sendAsync: function(payload, callback){
+    var self = this;
+    console.log("nonce",this._nonce);
+    
+    if(payload.method === "eth_sendTransaction"){
+      payload.method = "eth_sendRawTransaction";
 
+      var tx = new Tx();
 
+      _.extend(tx, {
+        nonce: this._nonce
+      }, payload.params[0]);
 
-console.log("address:",address);
-console.log("balance:", web3.fromWei(web3.eth.getBalance("0x"+address).toString(), "ether"), "ether");
-console.log("hexAmt:", hexAmt);
+      tx.sign(this._pk);
+      
+      payload.params = ['0x' + tx.serialize().toString('hex')];
+    }
 
-var tx = new Tx();
-tx.to = '0xffa19aeec96ef7b4a41448ba8fec37168edcab63';
-tx.gasPrice = '0x' + web3.eth.gasPrice.toString(16);
-tx.gasLimit = 21000;
-tx.nonce = "0x007";
+    console.log("raw payload:", payload);
 
-tx.value = new BN(new Buffer(hexAmt, 'hex')).add(tx.getUpfrontCost());
-tx.sign(privateKey);
-
-web3.eth.sendRawTransaction('0x' + tx.serialize().toString('hex'), function (err, r) {
-  console.log(err);
-  console.log(r);
+    HttpProvider.prototype.sendAsync.call(this, payload, function(){
+      self._updateNonce();
+      callback.apply(null,arguments); 
+    });
+  },
+  _updateNonce: function(){
+    this._nonce = parseInt(this.send({
+      jsonrpc: "2.0",
+      method: "eth_getTransactionCount",
+      params: [this._address, "latest"],
+      id: 1
+    }).result, 16) +2;
+  }
+  // ,
+  // send: function(payload, callback){
+  
+  // }
 });
+
+var privateKey = new Buffer('e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd108',
+                            'hex');
+
+var address = ethUtils.privateToAddress(privateKey).toString("hex");
+console.log(address);
+
+var web3 = new Web3(new RawProvider("http://46.101.91.161:8545", privateKey));
+//var web3 = new Web3(new Web3.providers.HttpProvider("http://46.101.91.161:8545"));
+
+//web3.eth.getTransactionCount('0xffa19aeec96ef7b4a41448ba8fec37168edcab63');
+
+
+web3.eth.sendTransaction({
+  from: "0x"+address,
+  to:'0xffa19aeec96ef7b4a41448ba8fec37168edcab63',
+  value: 100000
+}, function(err, res){
+  console.log(arguments);
+});
+
+
+
